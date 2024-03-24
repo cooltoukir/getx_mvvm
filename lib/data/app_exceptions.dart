350efcc0b88d4c986109a1980e3b1de424b72f2b
Md. Toukir Ahmed Mehedi <cooltoukir@gmail.com>
2024-03-24 14:32:08 +0600

    initial commit

---
+++ lib/data/app_exceptions.dart
@@ -0,0 +1,33 @@
+
+class AppExceptions implements Exception {
+
+  final _message;
+  final _prefix;
+
+  AppExceptions([this._message, this._prefix]);
+
+  @override
+  String toString() {
+    return 'AppExceptions{_message: $_message, _prefix: $_prefix}';
+  }
+}
+
+class InternetException extends AppExceptions {
+  InternetException([String? message]) : super(message, 'No internet');
+}
+
+class RequestTimeOut extends AppExceptions {
+  RequestTimeOut([String? message]) : super(message, 'Request time out');
+}
+
+class ServerException extends AppExceptions {
+  ServerException([String? message]) : super(message, 'Internet server error');
+}
+
+class InvalidUrlException extends AppExceptions {
+  InvalidUrlException([String? message]) : super(message, 'Invalid url');
+}
+
+class FetchDataException extends AppExceptions {
+  FetchDataException([String? message]) : super(message, '');
+}
